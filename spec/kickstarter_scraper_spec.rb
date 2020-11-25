c7916b2ac3c26b26964c2aeb22cebbff44d0bf32
tuckerTU <tuckerishere@gmail.com>
2020-11-25 13:37:47 -0700

    commit tree 13089a5c32114926f5c9c29f9ba02b99a17ef115

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -1,4 +1,36 @@
 describe "#create_project_hash" do
+  require 'nokogiri'
+  require 'pry'
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html) 
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+
+  # return the projects hash
+  projects
+end 
 
   it "can be called on with no errors" do
     expect {create_project_hash}.to_not raise_error
